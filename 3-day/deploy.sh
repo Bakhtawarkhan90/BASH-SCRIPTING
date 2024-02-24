a78f11a77fee9d1a78a93f574642c3b0fc3ba29b
BAKHTAWAR KHAN <bakhtawarkhan1367@gmail.com>
2024-02-24 11:40:39 +0530

    Update deploy.sh
    with docker-compose

--- 3-day/deploy.sh
+++ 3-day/deploy.sh
@@ -1,1 +1,47 @@
 
+#!/bin/bash
+ <<task
+deploy a app & handle errors
+task
+
+code_clone() {
+ echo "Cloning the code"
+ git clone https://github.com/LondheShubham153/django-notes-app.git
+}
+
+install_req() {
+ echo "Installing dependencies"
+ sudo apt-get install docker.io nginx -y docker-compose
+}
+
+req_restart() {
+ sudo chown $USER /var/run/docker.sock
+ #sudo systemctl enable docker
+ sudo systemctl enable nginx
+ #sudo systemctl restart docker
+}
+
+deploy() {
+ docker build . -t notes-app
+ docker-compose up -d
+ #docker run -d -p 8000:8000 notes-app:latest
+}
+echo "*************DEPLOYMENT STARTED**********"
+if ! code_clone; then
+ echo "the code dir already exists"
+ cd django-notes-app
+fi
+
+if ! install_req; then
+ echo "Installation Failed"
+ exit 1
+fi
+
+if ! req_restart; then
+ echo "system fault identified"
+ exit 1
+fi
+if ! deploy; then
+  echo "Sending-mail to Team Leader"
+  exit 1
+echo "************DEPLOYMENT DONE*************"
