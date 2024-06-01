cb1e3e63bfce9138cd36a96beb78aca5c54e93af
Ubuntu <ubuntu@ip-172-31-11-102.ap-south-1.compute.internal>
2024-06-01 11:12:58 +0000

    terraform

---
+++ terraform-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# INSTALLING TERRAFORM
+
+# Ensure that your system is up to date
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+# Download HashiCorp GPG key
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+# Verify the GPG key
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+
+# Add HashiCorp repository
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Update package lists
+sudo apt update
+
+# Install Terraform
+sudo apt-get install terraform
+
+# Display Terraform version"
+echo "*******************************************************************************************************"
+terraform --version
+echo "*******************************************************************************************************"
