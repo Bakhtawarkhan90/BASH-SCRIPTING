d296db43f60fed6f63d92f6ed166cb6eca61af8b
BAKHTAWAR KHAN <bakhtawarkhan1367@gmail.com>
2024-09-27 12:19:13 +0530

    Update swiggy.sh

--- swiggy.sh
+++ swiggy.sh
@@ -17,7 +17,7 @@ echo "*********************************************** Change directory to the ne
 cd Swiggy
 
 echo "*********************************************** start the Docker containers defined in the docker-compose.yml file ***************************************************"
-docker run -d --name swiggy -p 3000:3000 bakhtawar375/swiggy:latest
+sudo docker run -d --name swiggy -p 3000:3000 bakhtawar375/swiggy:latest
 
 echo "*********************************************** Wait for Docker containers to be fully up and running ***************************************************"
 sleep 5
@@ -26,6 +26,6 @@ echo "*********************************************** Clear the screen *********
 clear
 
 echo "*********************************************** Show all running Docker containers ***************************************************"
-docker ps
+sudo docker ps
 
 echo "*********************************************** Now You Can Access The App On $(curl -s ifconfig.me):3000 ***************************************"
