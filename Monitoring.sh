eeac90b82cb6b1f86e5a89a369d1b0feaa1a82cc
BAKHTAWAR KHAN <bakhtawarkhan1367@gmail.com>
2024-11-07 11:56:34 +0530

    Create Monitoring.sh

---
+++ Monitoring.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# NOTE: Before running this script you must have running k8s cluster
+
+# Installing Helm
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# Install Prometheus & Grafana
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add stable https://charts.helm.sh/stable
+helm repo update
+kubectl create namespace monitoring
+helm install kind-prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --set prometheus.service.nodePort=30000 --set prometheus.service.type=NodePort --set grafana.service.nodePort=31000 --set grafana.service.type=NodePort --set alertmanager.service.nodePort=32000 --set alertmanager.service.type=NodePort --set prometheus-node-exporter.service.nodePort=32001 --set prometheus-node-exporter.service.type=NodePort
+kubectl get svc -n monitoring
+kubectl get namespace
+
+# Open ports for Prometheus and  Grafana
+
+kubectl port-forward svc/kind-prometheus-kube-prome-prometheus -n monitoring 9090:9090 --address=0.0.0.0 &
+kubectl port-forward svc/kind-prometheus-grafana -n monitoring 31000:80 --address=0.0.0.0 &
+
+echo "NOW YOU CAN ACCESS PROMETHEUS ON PORT 9090 AND GRAFANA ON 31000"
